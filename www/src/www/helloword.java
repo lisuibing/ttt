628fcee2a7ee39837992cd45a0f534cf6727c6b5
86187 <86187@DESKTOP-QANQCHK.lan>
2020-10-07 09:44:06 +0800

    test

---
+++ www/src/www/helloword.java
@@ -0,0 +1,12 @@
+package www;
+
+public class helloword {
+
+	public static void main(String[] args) {
+		int aaa=4;
+		String name="hello!";
+		String word="lisuibing";
+		System.out.println(aaa+name+word);
+	}
+
+}
